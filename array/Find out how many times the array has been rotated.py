af3b8958ca729b91a9c4d26d8a708e08625680e3
Ayush <ayushsri1810@gmail.com>
2023-09-07 00:19:47 +0530

    find floor squar_root

---
+++ array/Find out how many times the array has been rotated.py
@@ -0,0 +1,14 @@
+# Problem Statement: Given an integer array arr of size N, sorted in ascending order (with distinct values). Now the array is rotated between 1 to N times which is unknown. Find how many times the array has been rotated. 
+
+
+
+# Examples
+# Example 1:
+# Input Format: arr = [4,5,6,7,0,1,2,3]
+# Result: 4
+# Explanation: The original array should be [0,1,2,3,4,5,6,7]. So, we can notice that the array has been rotated 4 times.
+
+# Example 2:
+# Input Format: arr = [3,4,5,1,2]
+# Result: 3
+# Explanation: The original array should be [1,2,3,4,5]. So, we can notice that the array has been rotated 3 times.
