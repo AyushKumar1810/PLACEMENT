202d3df65c6337faa225f3361678cd53aa938d6c
Ayush <ayushsri1810@gmail.com>
2024-03-11 16:08:56 +0530

    LC LC 31

---
+++ array/FARZ_Array/Medium/31. Next Permutation.py
@@ -0,0 +1,43 @@
+# A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+# For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+# The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+# For example, the next permutation of arr = [1,2,3] is [1,3,2].
+# Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+# While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+# Given an array of integers nums, find the next permutation of nums.
+
+# The replacement must be in place and use only constant extra memory.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [1,3,2]
+# Example 2:
+
+# Input: nums = [3,2,1]
+# Output: [1,2,3]
+# Example 3:
+
+# Input: nums = [1,1,5]
+# Output: [1,5,1]
+
+#NOTE: The concept is very much easy we will compare with arr[i] with it's arr[i+1]if it's greater than i+1 then it's ok we will decrease the pointer "i" if it's not then we will swap two indexed value .
+
+def nextPermutation(nums):
+    i=len(nums)-2
+    while i>=0 and nums[i] > nums[i+1]:
+        i=i-1
+    if i >0:
+        j=len(nums)-1
+        while nums[j] <=nums[i]:# we will continue checking till:left indexed value of greater than right value
+            j=j-1
+        nums[i] , nums[j] = nums[j] , nums[i]# if left indexed value of it is not  greater than right value , then we will swap to make larger array than previous .
+    nums[i+1:]=reversed(nums[i+1:])#That will only give next greater element of that
+# Example usage:
+nums = [1, 2, 3,4,5,6,7,8,9]
+nextPermutation(nums)
+print(nums)
