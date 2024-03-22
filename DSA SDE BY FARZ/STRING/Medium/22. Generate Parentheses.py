0bda16a4d35555c2a43e800d016b2b42e2f3420c
Ayush <ayushsri1810@gmail.com>
2024-03-23 01:20:21 +0530

    22-03-2024

---
+++ DSA SDE BY FARZ/STRING/Medium/22. Generate Parentheses.py
@@ -0,0 +1,35 @@
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Example 2:
+
+# Input: n = 1
+# Output: ["()"]
+ 
+
+# Constraints:
+
+# 1 <= n <= 8
+def generateParenthesis(n):
+    def backtrack(s, left, right):
+        if len(s) == 2 * n:
+            result.append(s)
+            return
+        if left < n:
+            backtrack(s + "(", left + 1, right)
+        if right < left:
+            backtrack(s + ")", left, right + 1)
+
+    result = []
+    backtrack("", 0, 0)
+    return result
+
+# Example usage:
+n = 3
+print(generateParenthesis(n))
+# Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
